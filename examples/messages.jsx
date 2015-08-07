bbbb055fa0421df1a9ce5e39d3dfaed190fd98c7
Dustin Getz <dustin.getz@gmail.com>
2015-08-07 15:29:20 -0400

    new message button in example

--- examples/messages.jsx
+++ examples/messages.jsx
@@ -14,7 +14,7 @@ function buildMessages (N) {
 var Messages = React.createClass({
     getInitialState: function() {
         return {
-            messages: buildMessages(25),
+            messages: buildMessages(50),
             isInfiniteLoading: false
         };
     },
@@ -24,34 +24,42 @@ var Messages = React.createClass({
         setTimeout(function() {
             this.setState({
                 isInfiniteLoading: false,
-                messages: this.state.messages.concat(buildMessages(25))
+                messages: this.state.messages.concat(buildMessages(50))
             });
         }.bind(this), 2500);
     },
 
     render: function() {
-        var elements = this.state.messages.map(function (record) {
+        var elements = this.state.messages.map(function (record, i) {
             var style = { height: undefined };
-            return <div className="infinite-list-item" style={style}>{record.text}</div>;
+            return <div className="infinite-list-item" data-i={i} style={style}>{i} {record.text}</div>;
         });
 
         var loadSpinner = <div className="infinite-list-item">Loading...</div>;
 
         // must pre-compute height of all elements
         return (
-            <Infinite
-                maxChildren={15}
-                elementHeight={100}
-                containerHeight={250}
-                // reverse={true}
-                infiniteLoadBeginBottomOffset={200}
-                onInfiniteLoad={this.handleInfiniteLoad}
-                loadingSpinnerDelegate={loadSpinner}
-                isInfiniteLoading={this.state.isInfiniteLoading}
-                timeScrollStateLastsForAfterUserScrolls={1000}>
-                {elements}
-            </Infinite>
+            <div className="Messages">
+                <Infinite
+                    maxChildren={15}
+                    containerHeight={500}
+                    // reverse={true}
+                    infiniteLoadBeginBottomOffset={200}
+                    onInfiniteLoad={this.handleInfiniteLoad}
+                    loadingSpinnerDelegate={loadSpinner}
+                    isInfiniteLoading={this.state.isInfiniteLoading}
+                    timeScrollStateLastsForAfterUserScrolls={1000}>
+                    {elements}
+                </Infinite>
+                <button onClick={this.receiveNewMessage}>Receive New Message</button>
+            </div>
         );
+    },
+
+    receiveNewMessage: function () {
+        this.setState({
+            messages: buildMessages(1).concat(this.state.messages)
+        });
     }
 });
 

d4ddd5bf9f1132889fb53cf910878ae877cff98c
Dustin Getz <dustin.getz@gmail.com>
2015-09-01 12:34:35 -0400

    Remove unused xor utility

--- src/utils/xor.js
+++
@@ -1,9 +0,0 @@
-function xor (a,b) {
-    // xor: if the inputs are different, return true
-    //     (true ^ true) === 1 => false
-    //     (true ^ false) === 1 => true
-    return (a ^ b) === 1;
-}
-
-
-module.exports = xor;

8e681fc18e091c1f823b496f027f744c0cf9fca9
Jesse Grant <jesse.grant@gmail.com>
2017-06-12 15:42:45 -0400

    React now gives warnings if you try to use React.createClass so switched to make a normal js class that extends React.Component. Also added an event handler so that a containing component can be notified of scroll events as they happen

--- src/react-chatview.js
+++ src/react-chatview.js
@@ -1,114 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import clone from 'lodash.clone';
+import PropTypes from 'prop-types';
 
-var supportsPassive = false;
+let supportsPassive = false;
+
 try {
-  var opts = Object.defineProperty({}, 'passive', {
+  let opts = Object.defineProperty({}, 'passive', {
     get() {
       supportsPassive = true;
     },
   });
   window.addEventListener('test', null, opts);
-} catch (e) { /* pass */ }
+} catch (e) { /* pass */
+}
 
-var ChatView = React.createClass({
+export default class ChatView extends React.Component {
 
-  propTypes: {
-    flipped: React.PropTypes.bool,
-    scrollLoadThreshold: React.PropTypes.number,
-    onInfiniteLoad: React.PropTypes.func.isRequired,
-    loadingSpinnerDelegate: React.PropTypes.element,
-    className: React.PropTypes.string
-  },
+  constructor(props) {
+    super(props);
 
-  getDefaultProps () {
-    return {
-      flipped: false,
-      scrollLoadThreshold: 10,
-      loadingSpinnerDelegate: <div/>,
-      className: ''
-    };
-  },
-
-  getInitialState () {
     this.rafRequestId = null; // for cleaning up outstanding requestAnimationFrames on WillUnmount
     this.scrollTop = 0; // regular mode initial scroll
     this.scrollHeight = undefined; // it's okay, this won't be read until the second render.
     // In flipped mode, we need to measure the scrollable height from the DOM to write to the scrollTop.
     // Flipped and regular measured heights are symmetrical and don't depend on the scrollTop
 
-    return {
+    this.state = {
       isInfiniteLoading: false
     };
-  },
+  }
 
-  componentWillUpdate (nextProps, nextState) {},
+  componentWillUpdate(nextProps, nextState) {
+  }
 
-  render () {
-    var displayables = clone(this.props.children);
+  render() {
+    let displayables = clone(this.props.children);
     if (this.props.flipped) {
       displayables.reverse();
     }
 
-    var loadSpinner = <div ref="loadingSpinner">
+    let loadSpinner = <div ref="loadingSpinner">
       {this.state.isInfiniteLoading ? this.props.loadingSpinnerDelegate : null}
     </div>;
 
     return (
-      <div className={this.props.className} ref="scrollable"
-           style={{overflowX: 'hidden', overflowY: 'scroll'}}>
-        <div ref="smoothScrollingWrapper">
-          {this.props.flipped ? loadSpinner : null}
-          {displayables}
-          {this.props.flipped ? null : loadSpinner}
+        <div className={this.props.className} ref="scrollable"
+             style={{ overflowX: 'hidden', overflowY: 'scroll' }}>
+          <div ref="smoothScrollingWrapper">
+            {this.props.flipped ? loadSpinner : null}
+            {displayables}
+            {this.props.flipped ? null : loadSpinner}
+          </div>
         </div>
-      </div>
     );
-  },
+  }
 
   // detect when dom has changed underneath us- either scrollTop or scrollHeight (layout reflow)
   // may have changed.
   onScroll() {
-    var domNode = ReactDOM.findDOMNode(this);
+    let domNode = ReactDOM.findDOMNode(this);
+
+    // if the containing component passed in an onscroll handler function as a property
+    if (this.props.handleScroll) {
+      this.props.handleScroll(domNode.scrollTop);
+    }
+
     if (domNode.scrollTop !== this.scrollTop) {
       if (this.shouldTriggerLoad(domNode)) {
         this.setState({ isInfiniteLoading: true });
-        var p = this.props.onInfiniteLoad();
+        let p = this.props.onInfiniteLoad();
         p.then(() => this.setState({ isInfiniteLoading: false }));
       }
       // the dom is ahead of the state
       this.updateScrollTop();
     }
-  },
+  }
 
-  pollScroll () {
-    this.onScroll()
+  pollScroll() {
+    this.onScroll();
     this.rafRequestId = window.requestAnimationFrame(this.pollScroll);
-  },
+  }
 
-  isPassedThreshold (flipped, scrollLoadThreshold, scrollTop, scrollHeight, clientHeight) {
+  isPassedThreshold(flipped, scrollLoadThreshold, scrollTop, scrollHeight, clientHeight) {
     return flipped
         ? scrollTop <= scrollLoadThreshold
         : scrollTop >= (scrollHeight - clientHeight - scrollLoadThreshold);
-  },
+  }
 
-  shouldTriggerLoad (domNode) {
-    var passedThreshold = this.isPassedThreshold(
+  shouldTriggerLoad(domNode) {
+    let passedThreshold = this.isPassedThreshold(
         this.props.flipped,
         this.props.scrollLoadThreshold,
         domNode.scrollTop,
         domNode.scrollHeight,
         domNode.clientHeight);
     return passedThreshold && !this.state.isInfiniteLoading;
-  },
+  }
 
-  componentDidMount () {
-    var scrollableDomEl = ReactDOM.findDOMNode(this);
+  componentDidMount() {
+    let scrollableDomEl = ReactDOM.findDOMNode(this);
 
     // If there are not yet any children (they are still loading),
     // this is a no-op as we are at both the top and bottom of empty viewport
-    var heightDifference = this.props.flipped
+    let heightDifference = this.props.flipped
         ? scrollableDomEl.scrollHeight - scrollableDomEl.clientHeight
         : 0;
 
@@ -119,36 +114,36 @@ var ChatView = React.createClass({
     // directly - that will break hardware accelerated scrolling. We poll it
     // with requestAnimationFrame instead.
     if (supportsPassive) {
-      scrollableDomEl.addEventListener('scroll', this.onScroll, {passive: true});
+      scrollableDomEl.addEventListener('scroll', this.onScroll.bind(this), { passive: true });
     } else {
       this.rafRequestId = window.requestAnimationFrame(this.pollScroll);
     }
-  },
+  }
 
-  componentWillUnmount () {
-    var scrollableDomEl = ReactDOM.findDOMNode(this);
-    scrollableDomEl.removeEventListener('scroll', this.onScroll, {passive: true});
+  componentWillUnmount() {
+    let scrollableDomEl = ReactDOM.findDOMNode(this);
+    scrollableDomEl.removeEventListener('scroll', this.onScroll.bind(this), { passive: true });
     window.cancelAnimationFrame(this.rafRequestId);
-  },
+  }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     this.updateScrollTop();
-  },
+  }
 
   updateScrollTop() {
-    var scrollableDomEl = ReactDOM.findDOMNode(this);
+    let scrollableDomEl = ReactDOM.findDOMNode(this);
 
     //todo this is only the happy path
-    var newScrollTop = scrollableDomEl.scrollTop + (this.props.flipped
-        ? scrollableDomEl.scrollHeight - (this.scrollHeight || 0)
-        : 0);
+    let newScrollTop = scrollableDomEl.scrollTop + (this.props.flipped
+            ? scrollableDomEl.scrollHeight - (this.scrollHeight || 0)
+            : 0);
 
     // if scrollHeightDifference is > 0 then something was removed from list
-    var scrollHeightDifference = this.scrollHeight ? this.scrollHeight - scrollableDomEl.scrollHeight : 0;
+    let scrollHeightDifference = this.scrollHeight ? this.scrollHeight - scrollableDomEl.scrollHeight : 0;
 
     // if something was removed from list we need to include this difference in new scroll top
     if (this.props.flipped && scrollHeightDifference > 0) {
-        newScrollTop += scrollHeightDifference;
+      newScrollTop += scrollHeightDifference;
     }
 
     if (newScrollTop !== scrollableDomEl.scrollTop) {
@@ -160,11 +155,24 @@ var ChatView = React.createClass({
 
     // Setting scrollTop can halt user scrolling (and disables hardware acceleration)
 
-    // Both cases - flipped and refular - have cases where the content expands in the proper direction,
-    // or the content expands in the wrong direciton. Either history or new message in both cases.
-    // We are only handling half of the cases. Or an image resized above or below us.
+    // Both cases - flipped and regular - have cases where the content expands in the proper direction,
+    // or the content expands in the wrong direction. Either history or new message in both cases.
+    // We are only handling half of the cases. Or an image re-sized above or below us.
   }
-});
+}
 
+ChatView.defaultProps = {
+  flipped: false,
+  scrollLoadThreshold: 10,
+  loadingSpinnerDelegate: <div/>,
+  className: ''
+};
 
-module.exports = ChatView;
+ChatView.propTypes = {
+  flipped: PropTypes.bool,
+  scrollLoadThreshold: PropTypes.number,
+  onInfiniteLoad: PropTypes.func.isRequired,
+  handleScroll: PropTypes.func,
+  loadingSpinnerDelegate: PropTypes.element,
+  className: PropTypes.string
+};
